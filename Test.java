df8ee6c4a54cb9f299fb91813db68b4860158a4d
Yarden Schorr <yardens01@gmail.com>
2021-03-17 19:25:53 +0200

    most updated git project

--- Test.java
+++ Test.java
@@ -28,10 +28,11 @@ public class Test {
         //write all FooterLinks
         FooterLinks(driver);
         footerLinkCheck(driver);
-
+        CurrencyChange(driver);
         uploadFiles(driver);
-        checkout(driver);
+       
         MyCartCheck(driver);
+        checkout(driver);
        
 
 
@@ -346,4 +347,30 @@ public static boolean isAlertPresent(WebDriver driver,Robot robot)  throws Inter
         return false; 
     }   // catch 
 }   // isAlertPresent()
+public static void CurrencyChange(WebDriver driver) {
+       
+         
+    //Clicking on the currency button and click euro and validate
+     driver.findElement(By.xpath("//strong[normalize-space()='$']")).click();
+     driver.findElement(By.xpath("//button[contains(text(),'€ Euro')]")).click();
+     String price = driver.findElement(By.xpath("//p[contains(text(),'472.33€')]")).getText();
+     if (price.equals("472.33€\n" +
+             "Ex Tax: 392.30€")) {
+         System.out.println("valid price");
+     } else {
+         System.out.println("failed");
+     }
+    //
+     driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div/div[2]/h4/a")).click();
+
+     String price1 = driver.findElement(By.xpath("//h2[contains(text(),'472.33€')]")).getText();
+     if (price1.equals("472.33€")) {
+         System.out.println("valid price");
+     } else {
+         System.out.println("failed");
+     }
+
+ }
+
+
 }
